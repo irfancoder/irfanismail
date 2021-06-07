a56f0a79302a393c5ce1741c9b2d576158c0b338
irfanismail <irfan.ismail96@gmail.com>
2021-06-07 15:05:58 +0800

    fix: contact form 1.1

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,13 +1,19 @@
 <template>
     <layout-base>
         <div class="w-full px-4 md:px-0">
-            <form name="contact" method="POST" data-netlify="true" action="/contact" @submit.prevent="submit">
+            <form id="contact" name="contact" method="post" data-netlify-honeypot="bot-field" data-netlify="true" action="/contact" @submit.prevent="submit">
                 <div class="form">
                     <h3 class="text-3xl md:text-5xl font-semibold pt-8 md:pt-16 pb-4 text-gray-900 dark:text-gray-50">What's up?</h3>
+                    <p hidden>
+                        <label>
+                            Don’t fill this out:{" "}
+                            <input name="bot-field" v-model="form" />
+                        </label>
+                    </p>
 
                     <label for="name">Name</label>
                     <input type="text" name="name" v-model="form.name" />
-                    <label for="name">Email</label>
+                    <label for="email">Email</label>
                     <input type="email" name="email" v-model="form.email" />
                     <label for="purpose">Type of Inquiry</label>
                     <select name="purpose" v-model="form.purpose">
